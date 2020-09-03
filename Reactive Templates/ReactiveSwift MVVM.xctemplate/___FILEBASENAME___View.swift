70c712f7b68126f2bfbfd718efa4ecd18a7f0ae5
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2020-09-03 17:24:04 +0300

    Added ReactiveSwift template

---
+++ Reactive Templates/ReactiveSwift MVVM.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,26 @@
+//
+// ___FILEHEADER___
+//
+
+import ReactiveCocoa
+import ReactiveSwift
+import UIKit
+
+final class ___VARIABLE_productName:identifier___View: UIViewController {
+    var viewModel: ___VARIABLE_productName:identifier___ViewModel!
+
+    override func loadView() {
+        view = UIView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        bindViewModel()
+    }
+
+    private func setupUI() { }
+
+    private func bindViewModel() { } 
+}
