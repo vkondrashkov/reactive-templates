3dff6193352e3c6ef1cda63961b7ef1a34fb23a3
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2020-09-03 17:23:46 +0300

    Added ReactiveKit template

---
+++ Reactive Templates/ReactiveKit MVVM.xctemplate/___FILEBASENAME___Router.swift
@@ -0,0 +1,17 @@
+//
+// ___FILEHEADER___
+//
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___RouterImpl {
+    private weak var viewController: UIViewController?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+}
+
+// MARK: - ___VARIABLE_productName:identifier___Router implementation
+
+extension ___VARIABLE_productName:identifier___RouterImpl: ___VARIABLE_productName:identifier___Router { }
---
+++ Reactive Templates/ReactiveKit MVVM.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,19 @@
+//
+// ___FILEHEADER___
+//
+
+import Bond
+import ReactiveKit
+
+final class ___VARIABLE_productName:identifier___ViewModelImpl: ___VARIABLE_productName:identifier___ViewModel {
+    private let interactor: ___VARIABLE_productName:identifier___Interactor
+    private let router: ___VARIABLE_productName:identifier___Router
+
+    init(
+        interactor: ___VARIABLE_productName:identifier___Interactor,
+        router: ___VARIABLE_productName:identifier___Router
+    ) {
+        self.interactor = interactor
+        self.router = router
+    }
+}
