5a1d5d120a492bf205948b32cc4ef7f0d3887540
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2020-09-04 15:52:22 +0300

    Updated install.sh, added uninstall.sh script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
+echo "[🚀 Reactive Templates] Remove existing reactive templates..."
+sudo rm -rf /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/Reactive\ Templates
 echo "[🚀 Reactive Templates] Copying files to Xcode Templates..."
 sudo cp -r Reactive\ Templates /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates
-echo "[🚀 Reactive Templates] Deleting supporting files..."
-cd ..
-rm -rf reactive-templates-master
 echo "[🚀 Reactive Templates] Done"
