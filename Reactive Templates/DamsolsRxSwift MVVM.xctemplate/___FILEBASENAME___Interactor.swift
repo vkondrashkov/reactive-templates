348b6754a3fd148c2c33076f1db85194350f5604
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-10-13 15:38:04 +0300

    Added DamsolsRxSwift template

---
+++ Reactive Templates/DamsolsRxSwift MVVM.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,12 @@
+import RxSwift
+
+final class ___VARIABLE_productName:identifier___InteractorImpl {
+    init() {
+
+    }
+}
+
+// MARK: - ___VARIABLE_productName:identifier___Interactor implementation
+
+extension ___VARIABLE_productName:identifier___InteractorImpl: ___VARIABLE_productName:identifier___Interactor { }
+
---
+++ Reactive Templates/DamsolsRxSwift MVVM.xctemplate/___FILEBASENAME___Router.swift
@@ -0,0 +1,15 @@
+import RxCocoa
+import RxSwift
+import UIKit
+
+final class ___VARIABLE_productName:identifier___RouterImpl {
+    private weak var view: UIViewController?
+
+    init(view: UIViewController) {
+        self.view = view
+    }
+}
+
+// MARK: - ___VARIABLE_productName:identifier___Router implementation
+
+extension ___VARIABLE_productName:identifier___RouterImpl: ___VARIABLE_productName:identifier___Router { }
